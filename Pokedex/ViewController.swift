20c982b2f47121449c2e0d579ecc33c091e5b136
mertcankiyak <reklammertcankiyak1@gmail.com>
2024-04-29 06:52:31 +0300

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by MERTCAN-KIYAK on 29.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
